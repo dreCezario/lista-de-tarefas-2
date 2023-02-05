d06b28a4826f5b6d46af3935daa49de090ab3318
André Cezario <87193294+dreCezario@users.noreply.github.com>
2023-02-05 09:57:16 -0300

    adicionando o script inicial, selecionando as variavéis e adicionando o primeiro evento submit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const todoForm = document.querySelector('#list-form')
+const todoInput = document.querySelector('#list-input')
+const todoList = document.querySelector('#todo-list')
+const editForm = document.querySelector('#edit-form')
+const editInput = document.querySelector('#edit-input')
+const cancelEditBtn = document.querySelector('#cancel-edit-btn')
+
+
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    console.log('Fooooooi')
+})
